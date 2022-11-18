e55e9ef122326e4395118f5003940b16e120a1f0
[jwhong70] <[jwhong70@gmail.com]>
2022-11-18 16:20:33 +0900

    #2-3 Server Setup

---
+++ schema.js
@@ -0,0 +1,13 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const typeDefs = mergeTypeDefs(loadedTypes);
+
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-import { ApolloServer, gql } from "apollo-server";
+require("dotenv").config();
+import express from "express";
+import { createServer } from "http";
+import { ApolloServer } from "apollo-server-express";
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from "apollo-server-core";
+import schema from "./schema";
+
+const PORT = process.env.PORT;
+async function startServer() {
+  const app = express();
+  const httpServer = createServer(app);
+  const apollo = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault(),
+    ],
+  });
+  await apollo.start();
+  apollo.applyMiddleware({ app, path: "/" });
+  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`
+  );
+}
+startServer();
