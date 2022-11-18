6a80acc9eb75264562bd3b2eb8b08c1716acae66
[jwhong70] <[jwhong70@gmail.com]>
2022-11-18 15:36:18 +0900

    #2-1 createAccount

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: { OR: [{ username }, { email }] },
+        });
+        if (existingUser) {
+          throw new Error("This username/email is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        await client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        return { ok: true };
+      } catch (e) {
+        return { ok: false, error: "Can't create account." };
+      }
+    },
+  },
+};
