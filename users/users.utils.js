e42d86ef90725b150841e307239a47cbf944be97
[jwhong70] <[jwhong70@gmail.com]>
2022-11-19 11:51:55 +0900

    #2-6 Protecting Resolvers

--- users/users.utils.js
+++ users/users.utils.js
@@ -17,3 +17,17 @@ export const getUser = async (token) => {
     return null;
   }
 };
+
+export function protectedResolver(ourResolver) {
+  return function (root, args, context, info) {
+    if (!context.loggedInUser) {
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        return { ok: false, error: "Please log in to perform this action." };
+      }
+    }
+    return ourResolver(root, args, context, info);
+  };
+}
