cea288faf6a142805e3b1b9677d8272d79ea31b0
[jwhong70] <[jwhong70@gmail.com]>
2022-11-22 16:30:41 +0900

    #3-10 See Feed & See Feeds

---
+++ photos/seeFeed/seeFeed.resolvers.js
@@ -0,0 +1,20 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolver((_, { offset }, { loggedInUser }) =>
+      client.photo.findMany({
+        take: 5,
+        skip: offset,
+        where: {
+          OR: [
+            { user: { followers: { some: { id: loggedInUser.id } } } },
+            { userId: loggedInUser.id },
+          ],
+        },
+        orderBy: { createdAt: "desc" },
+      })
+    ),
+  },
+};
