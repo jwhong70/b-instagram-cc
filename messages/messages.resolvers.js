58722ffd60c3c2ad96f220fc6482d048adce94c6
[jwhong70] <[jwhong70@gmail.com]>
2022-11-23 17:19:48 +0900

    #4-2 Send Message

---
+++ messages/messages.resolvers.js
@@ -0,0 +1,11 @@
+import client from "../client";
+
+export default {
+  Room: {
+    users: ({ id }) => client.room.findUnique({ where: { id } }).users(),
+    messages: ({ id }) => client.message.findMany({ where: { roomId: id } }),
+  },
+  Message: {
+    user: ({ id }) => client.message.findUnique({ where: { id } }).user(),
+  },
+};
